0c4b9b39bc58e00babd5db550bb20da05c118c28
Ramom Andrade <monmedeiros10@hotmail.com>
2020-11-22 00:41:57 -0300

    subida 1

---
+++ src/database/migrations/20201121023240_crate_users.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', function (table) {
+        /*table.increments();*/
+        table.string('id').primary();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.integer('idade');
+        table.string('empresa');
+        /*table.timestamps();*/
+    })
+  
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('users');  
+};
